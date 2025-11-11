4e2d385c2147a9f8c744658e91ae5cd86456f1c6
siddannth <sidhantprince72@gmail.com>
2025-11-11 12:02:24 -0600

    fixed app not found

--- app/not-found.js
+++ app/not-found.js
@@ -3,7 +3,7 @@ export default function NotFound()
     return(
         <main className="not-found">
         <h1>Not Found</h1>
-        <p>The page you're looking for does not exist</p>
+        <p>The page you are  looking for does not exist</p>
         </main>
     )
 }
